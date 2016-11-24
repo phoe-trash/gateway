ba94e06adefbe0e3631f4ff331ec8d8799c4dbc2
Michal Herda <phoe@openmailbox.org>
2016-11-24 13:51:48 +0100

    Refactor STANDARD-PASSWORD, begin refactoring away from dynamic vars

--- impl/standard-password.lisp
+++ impl/standard-password.lisp
@@ -7,27 +7,33 @@
 
 (defclass standard-password (password)
   ((%key :reader key-of
-	 :type string)
+         :type string)
    (%salt :reader salt
-	  :type string) 
+          :type string)
    (%iteration-count :reader iteration-count
-		     :type integer)))
+                     :type integer)))
 
-(defconstructor (standard-password passphrase %read-data)
+(defconstructor (standard-password passphrase
+                                   (iteration-count 1000)
+                                   (key-length 512)
+                                   (salt-length 64)
+                                   %read-data)
   (if %read-data
       (progn (check-type %read-data cons)
-	     (assert (= (length %read-data) 3))
-	     (check-type (first %read-data) string)
-	     (check-type (second %read-data) string)
-	     (check-type (third %read-data) integer))
-      (check-type passphrase string)) 
+             (assert (= (length %read-data) 3))
+             (check-type (first %read-data) string)
+             (check-type (second %read-data) string)
+             (check-type (third %read-data) integer))
+      (check-type passphrase string))
   (macrolet
       ((val (slot) `(slot-value standard-password ',slot))
        (bind (place)
-	 `(setf (values (val %key) (val %salt) (val %iteration-count)) ,place)))
+         `(setf (values (val %key) (val %salt) (val %iteration-count)) ,place)))
     (if %read-data
-	(bind (values-list %read-data))
-	(bind (derive-key passphrase)))))
+        (bind (values-list %read-data))
+        (bind (derive-key passphrase :iteration-count iteration-count
+                                     :key-length key-length
+                                     :salt-length salt-length)))))
 
 (defmethod password-matches-p ((password standard-password) (passphrase string))
   (let* ((salt (ironclad:hex-string-to-byte-array (salt password)))
@@ -41,15 +47,16 @@
 		     #:salt ,(salt password)
 		     #:iters ,(iteration-count password))))
 
-(defun derive-key (passphrase &key salt iteration-count key-length)
+(defun derive-key (passphrase &key salt
+                                (iteration-count 1000)
+                                (key-length 512)
+                                (salt-length 64))
   (let* ((kdf (ironclad:make-kdf 'ironclad:scrypt-kdf :digest :sha1))
-	 (passphrase (flex:string-to-octets passphrase :external-format :utf-8))
-	 (salt (or salt (ironclad:make-random-salt *password-salt-length*)))
-	 (iteration-count (or iteration-count *password-iteration-count*))
-	 (key-length (or key-length *password-key-length*))
-	 (key (ironclad:derive-key kdf passphrase salt iteration-count key-length))
-	 (result-key (ironclad:byte-array-to-hex-string key))
-	 (result-salt (ironclad:byte-array-to-hex-string salt)))
+         (passphrase (flex:string-to-octets passphrase :external-format :utf-8))
+         (salt (or salt (ironclad:make-random-salt salt-length)))
+         (key (ironclad:derive-key kdf passphrase salt iteration-count key-length))
+         (result-key (ironclad:byte-array-to-hex-string key))
+         (result-salt (ironclad:byte-array-to-hex-string salt)))
     (values result-key result-salt iteration-count)))
 
 (defun key-length (password)
--- util/constants.lisp
+++ util/constants.lisp
@@ -5,17 +5,17 @@
 
 (in-package #:gateway)
 
-(defconfig *password-iteration-count* 1000)
-(defconfig *password-key-length* 512)
-(defconfig *password-salt-length* 64)
+;; (defconfig *password-iteration-count* 1000)
+;; (defconfig *password-key-length* 512)
+;; (defconfig *password-salt-length* 64)
 
-(defconfig *cache-list* (make-hash-table))
-(defconfig *cache-lock* (make-lock))
+;; (defconfig *cache-list* (make-hash-table))
+;; (defconfig *cache-lock* (make-lock))
 ;; (defconfig *persona-cache* (make-hash-table :test 'equal) :cache :persona)
 ;; (defconfig *player-cache* (make-hash-table :test 'equal) :cache :player)
 ;; (defconfig *chat-cache* (make-hash-table :test 'equal) :cache :chat)
-(defconfig *connection-cache* (make-hash-table) :cache :connection)
+;; (defconfig *connection-cache* (make-hash-table) :cache :connection)
 
-(defvar *gem-n-handlers* (make-hash-table :test #'equal))
-(defvar *gem-e-handlers* (make-hash-table :test #'equal))
-(defvar *gem-i-handlers* (make-hash-table :test #'equal))
+;; (defvar *gem-n-handlers* (make-hash-table :test #'equal))
+;; (defvar *gem-e-handlers* (make-hash-table :test #'equal))
+;; (defvar *gem-i-handlers* (make-hash-table :test #'equal))
