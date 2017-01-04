4083932ca44f357842bfba9131bf04adc10e8907
Michał "phoe" Herda <phoe@openmailbox.org>
2017-01-04 23:55:35 +0100

    Fixes on logger

--- impl/standard-logger.lisp
+++ impl/standard-logger.lisp
@@ -8,20 +8,22 @@
 (defclass standard-logger (logger)
   ((%thread :accessor thread)
    (%name :accessor name)
+   (%stream :accessor stream-of)
    (%queue :accessor queue
            :initform (make-queue))))
 
 (defconstructor (standard-logger)
   (let ((name "Gateway - Logger"))
+    (join-thread (make-thread (lambda () (setf (stream-of standard-logger)
+                                               *standard-output*))))
     (setf (name standard-logger) name
           (thread standard-logger)
           (make-thread (curry #'%logger-loop standard-logger)
                        :name name))))
 
 (defun %logger-loop (logger)
-  (declare (optimize (debug 3)))
   (with-thread-handlers (logger)
-    (apply #'fformat t (pop-queue (queue logger)))))
+    (apply #'fformat (stream-of logger) (pop-queue (queue logger)))))
 
 (defun %note (args)
   (when (and (boundp '*logger*) *logger* (alivep *logger*))
