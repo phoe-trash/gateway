2e660a5eebed2f601c072223490e361be7da5ad3
Michał "phoe" Herda <phoe@openmailbox.org>
2017-01-25 21:44:08 +0100

    Add BKNR.DATASTORE

--- package.lisp
+++ package.lisp
@@ -4,9 +4,6 @@
 ;;;; package.lisp
 
 (defpackage #:gateway
-  (:shadowing-import-from #:closer-mop
-                          #:standard-generic-function #:defmethod #:defgeneric
-                          #:standard-method #:standard-class)
   (:use #:common-lisp
         #:closer-mop
         #:alexandria
@@ -17,6 +14,10 @@
         #:trivia
         #:cl-ppcre
         #:lparallel
-        #:lparallel.queue)
+        #:lparallel.queue
+        #:bknr.datastore)
+  (:shadowing-import-from #:closer-mop
+                          #:standard-generic-function #:defmethod #:defgeneric
+                          #:standard-method #:standard-class)
   (:shadowing-import-from #:1am
                           #:is #:signals #:*tests*))
