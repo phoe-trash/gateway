f5b5c9195fbebdad8c79a1546755f338cbe80303
Michał "phoe" Herda <phoe@teknik.io>
2018-04-03 23:06:38 +0200

    fix postmodern:with-connection position

--- db/connections.lisp
+++ db/connections.lisp
@@ -3,6 +3,21 @@
 ;;;; © Michał "phoe" Herda 2017
 ;;;; db/connections.lisp
 
+;; TODO remove this after https://bugs.launchpad.net/sbcl/+bug/1750466 is fixed
+(in-package :postmodern)
+
+(defmacro with-connection (spec &body body &environment env)
+  "Locally establish a database connection, and bind *database* to it."
+  (let ((connect-form (if (and (constantp spec env)
+                               (listp spec)
+                               (= 2 (length spec))
+                               (eq (first spec) 'quote))
+                          `(funcall #'connect ,@(second spec))
+                          `(apply #'connect ,spec))))
+    `(let ((*database* ,connect-form))
+       (unwind-protect (progn ,@body)
+         (disconnect *database*)))))
+
 (in-package #:gateway/db)
 
 (defmacro with-db (() &body body)
@@ -32,18 +47,3 @@ database."
   (with-db () (postmodern:query "SELECT 1;"))
   (with-test-db () (postmodern:query "SELECT 1;"))
   t)
-
-;; TODO remove this after https://bugs.launchpad.net/sbcl/+bug/1750466 is fixed
-(in-package :postmodern)
-
-(defmacro with-connection (spec &body body &environment env)
-  "Locally establish a database connection, and bind *database* to it."
-  (let ((connect-form (if (and (constantp spec env)
-                               (listp spec)
-                               (= 2 (length spec))
-                               (eq (first spec) 'quote))
-                          `(funcall #'connect ,@(second spec))
-                          `(apply #'connect ,spec))))
-    `(let ((*database* ,connect-form))
-       (unwind-protect (progn ,@body)
-         (disconnect *database*)))))
