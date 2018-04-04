928057f6803dfd203a03efd326c1c6e0506c2b6d
Michał "phoe" Herda <phoe@teknik.io>
2018-04-04 23:48:25 +0200

    add SQL commands: player-row-by-*

--- db/player.sql
+++ db/player.sql
@@ -1,4 +1,20 @@
 -- name: insert-player @execute
 -- Inserts a new player into the database.
 INSERT INTO player (player_name, email, display_name, passhash)
-    VALUES(:player_name, lower(:email), :display_name, :passhash);
+    VALUES(:player_name, lower(:email),
+           :display_name, decode(:passhash, 'hex'));
+
+-- name: player-row-by-name @row
+-- Retrieves a player whose data matches the arguments.
+SELECT * from player
+    WHERE player_name = ?
+
+-- name: player-row-by-email @row
+-- Retrieves a player whose data matches the arguments.
+SELECT * from player
+WHERE email = lower(?)
+
+-- name: player-row-by-display-name @row
+-- Retrieves a player whose data matches the arguments.
+SELECT * from player
+WHERE display_name = ?
