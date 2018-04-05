8de587f31c0cf0a26e6ffa1bcb29a47ae326f784
Michał "phoe" Herda <phoe@teknik.io>
2018-04-05 09:03:56 +0200

    function name

--- db/player.sql
+++ db/player.sql
@@ -4,17 +4,17 @@ INSERT INTO player (player_name, email, display_name, passhash)
     VALUES(:player_name, lower(:email),
            :display_name, decode(:passhash, 'hex'));
 
--- name: player-row-by-name @row
+-- name: select-player-by-name @row
 -- Retrieves a player whose data matches the arguments.
 SELECT * from player
     WHERE player_name = ?
 
--- name: player-row-by-email @row
+-- name: select-player-by-email @row
 -- Retrieves a player whose data matches the arguments.
 SELECT * from player
     WHERE email = lower(?)
 
--- name: player-row-by-display-name @row
+-- name: select-player-by-display-name @row
 -- Retrieves a player whose data matches the arguments.
 SELECT * from player
     WHERE display_name = ?
