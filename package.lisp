075fc29b7c7f82cb5de0de8c3e2ff7ba725e93d0
Michal Herda <michal.herda@ericsson.com>
2018-04-05 16:27:32 +0200

    cleanup

--- package.lisp
+++ package.lisp
@@ -6,10 +6,3 @@
 (defpackage #:gateway
   (:use #:common-lisp
         #:alexandria))
-
-(defpackage #:gateway/operations
-  (:use #:common-lisp
-        #:alexandria
-        #:phoe-toolbox
-        #:gateway/protocols
-        #:gateway/sql))
